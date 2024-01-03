10a071d76b2ec18b0591ccaebe8003a0fa86f592
Matt West <weststern@gmail.com>
2024-01-03 10:30:24 -0500

    updated contact form to send info to graphql backend

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '02520977cc1f78a43b11');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1737d0d3f9b6959ec5b3');
--- src/scripts/Contact.jsx
+++ src/scripts/Contact.jsx
@@ -1,46 +1,40 @@
-import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Grass from './Grass'
 // import Form from "./Form";
 import {useMutation,gql} from '@apollo/client'
+import React, { useState } from "react"
 
 
-    // const CONTACT_MUTATION = gql`
-    // mutation CreateSubmissionMutation($clientMutationId: String!, $email: String!, $subject: String!, $message: String!, $send_copy: String!){
-    //   createSubmission(input: {clientMutationId: $clientMutationId, email: $email, subject: $String, message: $String, send_copy: $send_copy}) {
-    //     success
-    //     data
-    //   }
-    // }`
 
 
-    // const [mutateFunction, { data, loading, error }] = useMutation(CONTACT_MUTATION);
+
+const CONTACT_MUTATION = gql`
+    mutation CreateSubmissionMutation($clientMutationId: String!, $email: String!, $subject: String!, $message: String!, $send_copy: Boolean){
+      createSubmission(input: {clientMutationId: $clientMutationId, email: $email, subject: $subject, message: $message, send_copy: $send_copy}) {
+        data
+        success
+      }
+    }`
+
+
 
 export default function Contact(){
     
 
+  const [emailValue, setEmailValue] = useState('')
+  const [subjectValue, setSubjectValue] = useState('')
+  const [sendCopyValue, setSendCopyValue] = useState()
+  const [messageValue, setMessageValue] = useState('')
 
-    const[formData, setFormData] = React.useState(
-        {email:"",subject:"",message:"",sendCopy:false}
-        )
 
-    function handleChange(event){
-        const {name,value,type,checked} = event.target
-        setFormData(prevFormData =>{
-            return{
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
-            }
-        })
-    }
+   const [CreateSubmissionMutation, { data, loading, error }] = useMutation(CONTACT_MUTATION);
+   
+  if (loading) return 'Submitting...';
 
-    function handleSubmit(event){
-        event.preventDefault()
-        console.log(formData);
-        // use this function to send to API
+  if (error) return `Submission error! ${error.message}`;
 
-    }
+
 
     return(
         <div className="gap-5 contact-container pl-14 font-display">
@@ -48,38 +42,39 @@ export default function Contact(){
         <Grass cssClass ='contact-container-grass' ></Grass>
         <h2 className="pb-16 mt-24 mr-20 text-4xl text-center contact-container-intro2">If you want to touch more grass, let me help. </h2>
         <div className="p-1 ml-16 mt-36 contact-container-intro">
+        <form onSubmit={e => {
+                e.preventDefault();
+                CreateSubmissionMutation({ variables: {      
+                        clientMutationId: 'example',
+                        email: emailValue ,
+                        subject: subjectValue ,
+                        send_copy: sendCopyValue ,
+                        message: messageValue }});
+                    }}>
             <h3 className="mt-20 mb-3 font-bold"> email</h3>
-            <form onSubmit ={handleSubmit}>
-                <input name="email" value={formData.email} onChange={handleChange} className='w-[402px] h-[43px]  mb-6  bg-zinc-300 rounded-[7px]'></input>
+     
+            <input  value={emailValue}  onChange={e => {setEmailValue(e.target.value)
+            }}  className='w-[402px] h-[43px]  mb-6  bg-zinc-300 rounded-[7px]'></input>
+            
             <h3 className="mb-3 font-bold"> subject</h3>
-                <input name="subject" value={formData.subject} onChange={handleChange} className=' pl-2 w-[402px] h-[43px]  mb-6 bg-zinc-300 rounded-[7px]'></input>
+                <input  value={subjectValue} onChange={e => {setSubjectValue(e.target.value)}}      
+                className=' pl-2 w-[402px] h-[43px]  mb-6 bg-zinc-300 rounded-[7px]'></input>
+            
             <h3 className="mb-3 font-bold"> message</h3>    
-            <textarea name = "message" value={formData.message} onChange={handleChange} id="message" className=' mb-3 pl-1 w-[402px] h-[86px]  bg-zinc-300 rounded-[7px]'></textarea>
+            <textarea value={messageValue} onChange={e => {setMessageValue(e.target.value)}}  
+             className=' mb-3 pl-1 w-[402px] h-[86px]  bg-zinc-300 rounded-[7px]'></textarea>
             <div>
-            <input name = 'sendCopy' className="mb-6" id='sendCopy' checked={formData.sendCopy} onChange={handleChange} type ='checkbox'></input>
-            <label htmlFor="sendCopy"> Send yourself a copy</label>
+            <input className="mb-6" id='sendCopyInput' value={sendCopyValue} onChange={e => {setSendCopyValue(!e.target.value)}}      
+            type ='checkbox'></input>
+            <label htmlFor='sendMessageInput'> Send yourself a copy</label>
             <div>
-            <button className= " focus:outline-none bg-[#7DDA28] hover:bg-[#5A9D1B] focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 " type="submit">Send Message</button>
+            <button className= " focus:outline-none bg-[#7DDA28] hover:bg-[#5A9D1B] focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 " type="submit">Send Message
+            </button>
             </div>
             </div>
-            </form>
+          </form>
        
         </div>
         <Footer></Footer>
         </div>
     )}
-
-
-
-// // Define mutation
-// const INCREMENT_COUNTER = gql`
-//   # Increments a back-end counter and gets its resulting value
-//   mutation IncrementCounter {
-//     currentValue
-//   }
-// `;
-
-// function MyComponent() {
-//   // Pass mutation to useMutation
-//   const [mutateFunction, { data, loading, error }] = useMutation(INCREMENT_COUNTER);
-// }
