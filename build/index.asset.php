46d234411fdfa000cc2134bf3918d49307c103c0
Matt West <weststern@gmail.com>
2024-03-27 11:00:09 -0400

    uploaded font's directly to wordpress media library— need to add additional font types

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'd004a14df5bd6807e5a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'c5d2323003e7bf124884');
